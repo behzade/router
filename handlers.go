9532d21944cf0fa62710665fcdc92a44c67f8d42
Behzad Ehsani <ehsani.eof@gmail.com>
2023-03-16 16:27:17 +0330

    fix: correct status code in response

--- handlers.go
+++ handlers.go
@@ -5,9 +5,11 @@ import (
 )
 
 func NotFoundHandlerFunc(w http.ResponseWriter, _ *http.Request) {
+    w.WriteHeader(http.StatusNotFound)
     w.Write([]byte("not found"))
 }
 
 func MethodNotAllowedHandlerFunc(w http.ResponseWriter, _ *http.Request) {
+    w.WriteHeader(http.StatusMethodNotAllowed)
     w.Write([]byte("method not allowed"))
 }

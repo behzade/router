35fdb4235c54488234e737b4e91b0f723014bc5f
Behzad Ehsani <ehsani.eof@gmail.com>
2023-03-16 12:58:34 +0330

    build: add basic router

--- route.go
+++ route.go
@@ -3,5 +3,44 @@ package router
 import "net/http"
 
 type Route struct {
-    handler http.Handler
+	handler http.Handler
+	name    *string
+}
+
+type Tree struct {
+	children map[string]*Tree
+	routes   map[string]*Route
+}
+
+func (root *Tree) insert(splitPath []string, route *Route, method string) {
+	if len(splitPath) == 0 {
+		root.routes[method] = route
+		return
+	}
+
+	child, ok := root.children[splitPath[0]]
+
+	if ok {
+		child.insert(splitPath[1:], route, method)
+		return
+	}
+
+	child = &Tree{map[string]*Tree{}, nil}
+	child.insert(splitPath[1:], route, method)
+	root.children[splitPath[0]] = child
+}
+
+func (root *Tree) find(splitPath []string, method string) (*Route, bool) {
+	if len(splitPath) == 0 {
+		route, ok := root.routes[method]
+		return route, ok
+	}
+
+	child, ok := root.children[splitPath[0]]
+
+	if ok {
+		return child.find(splitPath[1:], method)
+	}
+
+	return nil, false
 }
--- router.go
+++ router.go
@@ -1,6 +1,36 @@
 package router
 
+import "net/http"
+
 type Router struct {
-    routes []*Route
-    middleware []Middleware
+	NotFoundHandler         http.Handler
+	MethodNotAllowedHandler http.Handler
+
+	routeTree               *Tree
+	middleware              []Middleware
+}
+
+func New() *Router {
+	return &Router{}
+}
+
+func (r *Router) addRoute(path string, method string, handler http.Handler) {
+	splitPath, _ := ParsePath(path)
+
+	r.routeTree.insert(splitPath, &Route{handler, method})
+}
+
+func (r *Router) resolve(path string, method string) (http.Handler, bool) {
+	splitPath, _ := ParsePath(path)
+	route, ok := r.routeTree.find(splitPath)
+
+	if !ok {
+		return r.NotFoundHandler, false
+	}
+
+	if route.method != method {
+		return r.MethodNotAllowedHandler, false
+	}
+
+	return route.handler, true
 }

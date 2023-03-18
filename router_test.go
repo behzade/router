3e01308d14087c3c08e9cfdbf8154f5fa6686e04
Behzad Ehsani <ehsani.eof@gmail.com>
2023-03-18 23:13:43 +0330

    chore: add router test

---
+++ router_test.go
@@ -0,0 +1,63 @@
+package router
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"testing"
+)
+
+// taken from httprouter
+func TestRouter(t *testing.T) {
+	var get, post, put, patch, delete bool
+
+	router := New()
+	router.GET("/GET", http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
+		get = true
+	}))
+	router.POST("/POST", http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
+		post = true
+	}))
+	router.PUT("/PUT", http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
+		put = true
+	}))
+	router.PATCH("/PATCH", http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
+		patch = true
+	}))
+	router.DELETE("/DELETE", http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
+		delete = true
+	}))
+
+	w := testResponeWriter{bytes.Buffer{}, http.Header{}}
+
+	r, _ := http.NewRequest(http.MethodGet, "/GET", nil)
+	router.ServeHTTP(w, r)
+	if !get {
+		t.Error("routing GET failed")
+        fmt.Printf("w: %v\n", w)
+	}
+
+	r, _ = http.NewRequest(http.MethodPost, "/POST", nil)
+	router.ServeHTTP(w, r)
+	if !post {
+		t.Error("routing POST failed")
+	}
+
+	r, _ = http.NewRequest(http.MethodPut, "/PUT", nil)
+	router.ServeHTTP(w, r)
+	if !put {
+		t.Error("routing PUT failed")
+	}
+
+	r, _ = http.NewRequest(http.MethodPatch, "/PATCH", nil)
+	router.ServeHTTP(w, r)
+	if !patch {
+		t.Error("routing PATCH failed")
+	}
+
+	r, _ = http.NewRequest(http.MethodDelete, "/DELETE", nil)
+	router.ServeHTTP(w, r)
+	if !delete {
+		t.Error("routing DELETE failed")
+	}
+}

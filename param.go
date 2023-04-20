2fb3fc847ba20a5ef1a1ced473d22eb18b03fa05
Behzad Ehsani <ehsani.eof@gmail.com>
2023-04-20 15:04:08 +0330

    Revert "wip: testing the effect of to string on byte for params"
    This reverts commit f49078fbfab185c14a031f3b7d41a1a81ef03838.

--- param.go
+++ param.go
@@ -2,17 +2,17 @@ package router
 
 type Param struct {
 	key   string
-	value string
+	value []byte
 }
 
 type Params []Param
 
-func (p *Params) Get(key string) string {
+func (p *Params) Get(key string) []byte {
 	for _, v := range *p {
 		if v.key == key {
 			return v.value
 		}
 	}
 
-	return ""
+	return nil
 }
